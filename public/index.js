f229a81956fd0fe061063a2910055c7cfe4cf271
harshitagupta11 <33648470+harshitagupta11@users.noreply.github.com>
2019-01-08 19:37:41 +0530

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,40 @@
+var socket;
+var message;
+var handle;
+var button;
+var output;
+var feedback;
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1');
+    background(170);
+    socket = io.connect('http://localhost:8000');
+    message = select('#message');
+    handle = select('#handle');
+    send = select('#send');
+    output = select('#output');
+    feedback = select('#feedback');
+
+    send.mousePressed(sendMessage);
+    //socket.on('chat', msgs);
+}
+
+function sendMessage() {
+    var data = {
+        message: message.value(),
+        handle: handle.value()
+
+    };
+    console.log(data.message);
+    socket.emit('chat', data);
+    socket.on('chat', msgs);
+
+    function msgs(data) {
+        // var msg = createP(data.handle);
+        //msg.html(data.handle);
+        //msg.parent('output');
+        output.html("<p><strong>" + data.handle + ":</strong>" + data.message + "</p>", true);
+    }
+}
---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var socket = require('socket.io');
+var app = express();
+var server = app.listen(8000);
+app.use(express.static('public'));
+console.log('server is running at port 8000');
+var io = socket(server);
+io.sockets.on('connection', function (socket) {
+    console.log('New Connection:' + socket.id);
+    socket.on('chat', chatmsg);
+
+    function chatmsg(data) {
+        sockets.broadcast.emit('chat', data);
+        console.log('emits');
+    }
+});
